bb042f549b65ef0201a0548f2a0ba3771700bdc7
Attila Sági <attila.sagi@itegration.com>
2024-10-28 14:21:26 +0100

    Add website to admin form, and update handling

---
+++ Model/ResourceModel/ReadHandler.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Copyright (c) 2024 Attila Sagi
+ * @license http://www.opensource.org/licenses/mit-license.html  MIT License
+ */
+
+declare(strict_types=1);
+
+namespace Space\SalesCountdown\Model\ResourceModel;
+
+use Magento\Framework\EntityManager\Operation\AttributeInterface;
+use Space\SalesCountdown\Model\ResourceModel\Rule as ResourceRule;
+use Magento\Framework\EntityManager\MetadataPool;
+
+class ReadHandler implements AttributeInterface
+{
+    /**
+     * @var ResourceRule
+     */
+    protected ResourceRule $ruleResource;
+
+    /**
+     * @var MetadataPool
+     */
+    protected MetadataPool $metadataPool;
+
+    /**
+     * Constructor
+     *
+     * @param ResourceRule $ruleResource
+     * @param MetadataPool $metadataPool
+     */
+    public function __construct(
+        ResourceRule $ruleResource,
+        MetadataPool $metadataPool
+    ) {
+        $this->ruleResource = $ruleResource;
+        $this->metadataPool = $metadataPool;
+    }
+
+    /**
+     * Execute
+     *
+     * @param string $entityType
+     * @param array $entityData
+     * @param array $arguments
+     * @return array
+     * @throws \Exception
+     */
+    public function execute($entityType, $entityData, $arguments = []): array
+    {
+        $linkField = $this->metadataPool->getMetadata($entityType)->getLinkField();
+        $entityId = $entityData[$linkField];
+
+        $entityData['website_ids'] = $this->ruleResource->getWebsiteIds($entityId);
+
+        return $entityData;
+    }
+}
--- Model/ResourceModel/Rule/Collection.php
+++ Model/ResourceModel/Rule/Collection.php
@@ -8,15 +8,23 @@ declare(strict_types=1);
 
 namespace Space\SalesCountdown\Model\ResourceModel\Rule;
 
-use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
+use Magento\Framework\Event\ManagerInterface;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Rule\Model\ResourceModel\Rule\Collection\AbstractCollection;
 use Space\SalesCountdown\Model\Rule;
 use Space\SalesCountdown\Model\ResourceModel\Rule as ResourceRule;
 
 /**
  * @SuppressWarnings(PHPMD.CamelCasePropertyName)
+ * @SuppressWarnings(PHPMD.LongVariable)
  */
 class Collection extends AbstractCollection
 {
+    /**
+     * @var array
+     */
+    protected $_associatedEntitiesMap; // NOSONAR
+
     /**
      * Identifier field name for collection items
      *
@@ -36,7 +44,33 @@ class Collection extends AbstractCollection
      *
      * @var string
      */
-    protected $_eventObject = 'countdown_rule_collection'; // NOSONAR
+    protected $_eventObject = 'sales_countdown_rule'; // NOSONAR
+
+    /**
+     * @param \Magento\Framework\Data\Collection\EntityFactoryInterface $entityFactory
+     * @param \Psr\Log\LoggerInterface $logger
+     * @param \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy
+     * @param ManagerInterface $eventManager
+     * @param \Magento\Framework\DB\Adapter\AdapterInterface|null $connection
+     * @param \Magento\Framework\Model\ResourceModel\Db\AbstractDb|null $resource
+     */
+    public function __construct(
+        \Magento\Framework\Data\Collection\EntityFactoryInterface $entityFactory,
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy,
+        ManagerInterface $eventManager,
+        \Magento\Framework\DB\Adapter\AdapterInterface $connection = null, \Magento\Framework\Model\ResourceModel\Db\AbstractDb $resource = null
+    ) {
+        parent::__construct(
+            $entityFactory,
+            $logger,
+            $fetchStrategy,
+            $eventManager,
+            $connection,
+            $resource
+        );
+        $this->_associatedEntitiesMap = $this->getAssociatedEntitiesMap();
+    }
 
     /**
      * Define resource model
@@ -47,5 +81,74 @@ class Collection extends AbstractCollection
     protected function _construct(): void
     {
         $this->_init(Rule::class, ResourceRule::class);
+    }
+
+    /**
+     * Map data for associated entities
+     *
+     * @param string $entityType
+     * @param string $objectField
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @return void
+     */
+    protected function mapAssociatedEntities(
+        string $entityType,
+        string $objectField
+    ): void {
+        if (!$this->_items) {
+            return;
+        }
+
+        $entityInfo = $this->_getAssociatedEntityInfo($entityType);
+        $ruleIdField = $entityInfo['rule_id_field'];
+        $entityIds = $this->getColumnValues($ruleIdField);
+
+        $select = $this->getConnection()->select()->from(
+            $this->getTable($entityInfo['associations_table'])
+        )->where(
+            $ruleIdField . ' IN (?)',
+            $entityIds
+        );
+
+        $associatedEntities = $this->getConnection()->fetchAll($select);
+
+        array_map(function ($associatedEntity) use ($entityInfo, $ruleIdField, $objectField) {
+            $item = $this->getItemByColumnValue($ruleIdField, $associatedEntity[$ruleIdField]);
+            $itemAssociatedValue = $item->getData($objectField) === null ? [] : $item->getData($objectField);
+            $itemAssociatedValue[] = $associatedEntity[$entityInfo['entity_id_field']];
+            $item->setData($objectField, $itemAssociatedValue);
+        }, $associatedEntities);
+    }
+
+    /**
+     * Perform operations after collection load
+     *
+     * @return $this
+     * @throws LocalizedException
+     * @SuppressWarnings(PHPMD.CamelCaseMethodName)
+     */
+    protected function _afterLoad(): static
+    {
+        $this->mapAssociatedEntities('website', 'website_ids');
+
+        $this->setFlag('add_websites_to_result', false);
+        return parent::_afterLoad();
+    }
+
+    /**
+     * Getter for _associatedEntitiesMap property
+     *
+     * @return array
+     * @deprecated 100.1.0
+     */
+    private function getAssociatedEntitiesMap()
+    {
+        if (!$this->_associatedEntitiesMap) {
+            $this->_associatedEntitiesMap = \Magento\Framework\App\ObjectManager::getInstance()
+                // phpstan:ignore
+                ->get(\Space\SalesCountdown\Model\ResourceModel\Rule\AssociatedEntityMap::class)
+                ->getData();
+        }
+        return $this->_associatedEntitiesMap;
     }
 }
