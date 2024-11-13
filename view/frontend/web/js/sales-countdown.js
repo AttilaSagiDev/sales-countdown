d425a5d83163df1caf7c004b1869dfefe2608992
Attila Sági <attila.sagi@itegration.com>
2024-11-13 14:21:34 +0100

    Add basic countdown to frontend

--- view/frontend/web/js/sales-countdown.js
+++ view/frontend/web/js/sales-countdown.js
@@ -70,9 +70,52 @@ define([
                 && result.hasOwnProperty('countdown_end_date')
                 && result.countdown_end_date !== ''
             ) {
-                $(divSelector).html(result.countdown_end_date);
+                this._displayCountdown(result.countdown_end_date, divSelector);
                 $(divSelector).removeClass('no-display');
             }
+        },
+
+        /**
+         * Display countdown
+         *
+         * @param {String} countdownEndDate
+         * @param {String} divSelector
+         * @private
+         * @return void
+         */
+        _displayCountdown: function (countdownEndDate, divSelector) {
+            const countDownDate = new Date(countdownEndDate).getTime();
+            $(divSelector).html(this._calculateCountdown(countDownDate).countdownMessage);
+
+            let interVal = setInterval(function () {
+                let countdownMessage = this._calculateCountdown(countDownDate);
+                $(divSelector).html(countdownMessage.countdownMessage);
+                if (countdownMessage.distance < 0) {
+                    clearInterval(interVal);
+                    $(divSelector).removeClass('no-display');
+                }
+            }.bind(this), 1000);
+        },
+
+        /**
+         * Calculate countdown
+         *
+         * @param {Number} countDownDate
+         * @return {Object} {{countdownMessage: string, distance: number}}
+         * @private
+         */
+        _calculateCountdown: function (countDownDate) {
+            const now = new Date().getTime();
+            const distance = countDownDate - now;
+            const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            return {
+                countdownMessage: `${days}d ${hours}h ${minutes}m ${seconds}s`,
+                distance: distance
+            };
         }
     });
 
